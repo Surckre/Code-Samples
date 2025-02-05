3b11b1295a4c9cd1085c30aaef3613c33fc84262
Surckre <NotMeLuc@gmail.com>
2025-02-05 19:12:06 +0100

    Create basic Calculator.c

---
+++ C/basic Calculator.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	double num1;
+	double num2;
+	char opr;
+
+	printf("Enter first Number: ");
+	scanf("%lf", &num1);
+
+	printf("Enter Operator: ");
+	scanf(" %c",&opr);
+
+	printf("\nEnter second Number: ");
+	scanf("%lf",&num2);
+
+	if (opr == '+') {
+		double res = num1 + num2;
+		printf("%.2lf", res);
+		
+	}
+	else if (opr == '-') {
+		double res = num1 - num2;
+		printf("%.2lf", res);
+		
+	}
+	else if (opr == '*') {
+		double res = num1 * num2;
+		printf("%.2lf", res);
+		
+	}
+	else if (opr == '/') {
+		double res = num1 / num2;
+		printf("%.2lf", res);
+	}
+	else {
+		printf("The operator %c is not valid",opr);
+	}
+}
